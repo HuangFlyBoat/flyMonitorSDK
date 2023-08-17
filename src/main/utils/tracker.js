8342041c4c168d6528c327bd26b69f30e9ff2ef0
flyboahuang <flyboahuang@tencent.com>
2023-08-17 15:05:28 +0800

    修复原有请求逻辑短时间内发送多次请求会中断的问题

--- src/main/utils/tracker.js
+++ src/main/utils/tracker.js
@@ -19,7 +19,6 @@ class SendTracker {
      */
     constructor(reportUrl, source) {
         this.url = reportUrl;
-        this.xhr = new XMLHttpRequest;
         this.source = source || '';
     }
     // https://help.aliyun.com/zh/sls/developer-reference/putwebtracking#reference-354467 阿里云日志请求格式
@@ -37,16 +36,17 @@ class SendTracker {
                 }
             }
         }
-        this.xhr.open('POST', this.url, true);
+        const xhr = new XMLHttpRequest;
+        xhr.open('POST', this.url, true);
         let body = JSON.stringify(log);
         console.log('log', log);
-        this.xhr.setRequestHeader('Content-Type', 'application/json');
-        this.xhr.setRequestHeader('x-log-apiversion', '0.6.0');
-        this.xhr.setRequestHeader('x-log-bodyrawsize', body.length);
-        this.xhr.onerror = function (error) {
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('x-log-apiversion', '0.6.0');
+        xhr.setRequestHeader('x-log-bodyrawsize', body.length);
+        xhr.onerror = function (error) {
             console.log('error', error)
         };
-        this.xhr.send(body);
+        xhr.send(body);
     }
 }
 
